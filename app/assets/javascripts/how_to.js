c3d11d28eafbf7e5fa184a805a6aaa4b0978be33
miyaosummer <yoshihikon9@gmail.com>
2020-07-08 20:57:08 +0900

    新規登録画面のビューを作成

--- app/assets/javascripts/how_to.js
+++ app/assets/javascripts/how_to.js
@@ -1,5 +1,4 @@
 $(function(){
-  console.log("OK")
   $.when(
     $(".howto-main").animate({
       'margin':'0px',
@@ -9,11 +8,9 @@ $(function(){
     $(".howto-main__notise__first").animate({
       'opacity':'1',
     },1000);
-    console.log("終わった")
     $(".howto-main__img__second").animate({
       'opacity':'1',
     },1000);
-    console.log("できたはず")
   });  
 });
 
